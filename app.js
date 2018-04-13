3b8e0e692bf90d675ef7d1ba5a3ebf24ca6e03ff
kattisarika <sarika.katti@gmail.com>
2018-04-13 00:03:11 -0700

    updating the repo

--- app.js
+++ app.js
@@ -6,6 +6,9 @@ var cookieParser = require('cookie-parser');
 var expressSession = require('express-session');
 var passport = require('passport');
 var passportLocal = require('passport-local');
+var bcrypt = require('bcryptjs');
+var passport = require('passport');
+var passportLocal = require('passport-local');
 var mongoose = require('mongoose');
 var bodyParser = require('body-parser');
 
@@ -25,6 +28,9 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(expressSession({secret:'anystringoftext',saveUninitialized:true,resave:true}))
+
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
--- app/routes.js
+++ app/routes.js
@@ -1,27 +1,71 @@
 var User = require('./models/user');
 module.exports = function(app){
+	
+
+
 	app.get('/', function(req, res){
 		res.render("index.ejs")
-	});
+});
 
 	app.get('/contact',function(req, res){
 		res.render("contact.ejs")
 	});
 
+
+   
 	app.get('/login',function(req,res){
 		res.render("login.ejs")
 
 	});
 
 	app.get('/sign', function(req,res){
-		res.render("sign.ejs");
+		console.log("DO I GET REGISTER GET CALLED ");
+    	res.render('sign.ejs');
 	});
 
-  app.get('/register',function(req,res){
-  		res.send("success");
-		res.render("register.ejs")
+    app.post('/sign', function(req, res){
+    	console.log(req);
+		 if (req.body.name &&
+        req.body.email &&
+        req.body.password &&
+        req.body.confpassword)
 
+
+
+        if (req.body.password !== req.body.confpassword) {
+            var err = new Error("passwords do not match");
+            err.status = 400;
+            res.send(err);
+        } else {
+
+            var newUser = new User({
+                username: req.body.name,
+                email: req.body.email,
+                password: req.body.password
+            });
+            //  console.log("All data captured in backend" + newUser.username + "," + newUser.email + "," + newUser.password);
+
+            User.createUser(newUser, function(err, user) {
+                if (err) throw err;
+                res.redirect("/login");
+
+            });
+
+        }
 	});
+
+
+	app.get('/:username/:password', function(req, res){
+		var newUser = new User();
+		newUser.local.username = req.params.username;
+		newUser.local.password = req.params.password;
+		console.log(newUser.local.username + " " + newUser.local.password);
+		newUser.save(function(err){
+			if(err)
+				throw err;
+		});
+		res.send("Success!");
+	})
 
 
 	app.get('/yoursong', function(req,res){
@@ -36,17 +80,4 @@ module.exports = function(app){
 		res.render("songlist.ejs");
 	})
 
-
-
-	app.get('/:username/:password', function(req, res){
-		var newUser = new User();
-		newUser.local.username = req.params.username;
-		newUser.local.password = req.params.password;
-		console.log(newUser.local.username + " " + newUser.local.password);
-		newUser.save(function(err){
-			if(err)
-				throw err;
-		});
-		res.send("Success!");
-	})
 }
